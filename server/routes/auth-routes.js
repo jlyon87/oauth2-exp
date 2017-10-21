b9a761472b083170dea870d2803b894cd9194f8c
jlyonas <jlyon@acumensolutions.com>
2017-10-21 17:42:11 -0400

    learning express router req props

--- server/routes/auth-routes.js
+++ server/routes/auth-routes.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const request = require("request");
+
 const esiApi = require("../esi-api.js");
 
 module.exports = function(app) {
@@ -9,7 +11,13 @@ module.exports = function(app) {
 	});
 
 	app.get("/auth", function(req, res) {
-		console.log("GET we back from login")
+		console.log("GET we back from login");
+
+		console.log("req.url", req.url);
+		console.log("req.originalUrl", req.originalUrl);
+		console.log("req.query", req.query);
+
+		res.redirect("/");
 	});
 
 	app.post("/auth", function(req, res) {
