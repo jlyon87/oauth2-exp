c71aa9d56b1fdd7c4b3e268869fa55c2f623ec98
jlyonas <jlyon@acumensolutions.com>
2017-10-21 13:25:14 -0400

    add static public routes

--- server/routes/html-routes.js
+++ server/routes/html-routes.js
@@ -3,9 +3,6 @@ const path = require("path");
 module.exports = function(app) {
 
 	app.get("/", function(req, res) {
-		console.log( "home __dirname", __dirname );
-		const filePath = path.join( __dirname, "..", "..", "public", "index.html");
-		console.log("index.html path ", filePath);
-		res.sendFile(filePath);
+		res.sendFile(path.join( __dirname, "..", "..", "public", "index.html"));
 	});
 };
--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
+const path = require("path");
 const express = require("express");
 const request = require("request");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+app.use("/public", express.static(path.join(__dirname, "..", "public")));
 
 require("./routes/html-routes.js")(app);
 
