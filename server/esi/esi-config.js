fd9d400cfe097297a284953d8acf4636f378f2d1
jlyonas <jlyon@acumensolutions.com>
2018-01-24 08:53:55 -0500

    extract scopes from creds obj

--- server/esi/esi-config.js
+++ server/esi/esi-config.js
@@ -1,9 +1,11 @@
+const scopes = process.env.ESI_SCOPES.split(" ").join("%20");
+
 const creds = {
 	clientId: process.env.ESI_CLIENT_ID,
 	secretKey: process.env.ESI_SECRET_KEY,
 	callbackUrl: process.env.ESI_CALLBACK_URL,
 	state: process.env.ESI_STATE,
-	scopes: process.env.ESI_SCOPES
+	scopes
 };
 
 module.exports = creds;
