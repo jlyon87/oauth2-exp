0e970aa1998f09f9541fabde7bfb97daf6ab6bba
jlyonas <jlyon@acumensolutions.com>
2018-01-24 08:26:36 -0500

    refactor esi-config

--- server/esi/esi-config.js
+++ server/esi/esi-config.js
@@ -1,34 +1,9 @@
-const fs = require("fs");
-
 const creds = {
 	clientId: process.env.ESI_CLIENT_ID,
 	secretKey: process.env.ESI_SECRET_KEY,
 	callbackUrl: process.env.ESI_CALLBACK_URL,
 	state: process.env.ESI_STATE,
 	scopes: process.env.ESI_SCOPES
-};
-
-const getEsiScopes = () => {
-	return new Promise((resolve, reject) => {
-		fs.readFile("data/esi-scopes.txt", "utf8", (err, data) => {
-			if (err) reject(err);
-
-			resolve(data.trim().split(" "));
-		});
-	});
-};
-
-const init = () => {
-	return new Promise((resolve, reject) => {
-		getEsiScopes().then((scopes) => {
-			if (scopes) {
-				creds.scopes = scopes
-				resolve(scopes);
-			}
-
-			reject(new Error("Error retrieving scopes"));
-		});
-	});
 };
 
 module.exports = creds;
