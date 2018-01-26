24576cf9d9f732f78dc695f7b317e44240b3cf62
jlyonas <jlyon@acumensolutions.com>
2018-01-26 09:42:10 -0500

    refactor esi private middleware to multiple functions

--- server/routes/esi/esi-private.js
+++ server/routes/esi/esi-private.js
@@ -6,41 +6,45 @@ const esiWallet = require("./esi-wallet");
 const { addSeconds, calcRemainingTime } = require("../../plugins/date-helper");
 const esiApi = require("../../esi/esi-auth");
 
-const accessTokenIsValid = (req, res, next) => {
-	console.log("*** confirming token validity ***");
+const refreshAuth = (req, res, next) => {
+	esiApi.refreshAccess(req.session.esi.refresh_token)
+	.then(esiRes => {
+		if(esiRes.status === 200 && esiRes.statusText === "OK") {
+			req.session.esi = esiRes.data;
+			req.session.esi.expiryTime = addSeconds(new Date(), esiRes.data.expires_in);
 
-	if(!req.session.esi) {
-		res.status(403).send("Invalid access");
-	} else {
-
-		const msRemaining = calcRemainingTime(new Date(req.session.esi.expiryTime));
-
-		if(msRemaining > 60000) {
 			next();
+
 		} else {
-
-			esiApi.refreshAccess(req.session.esi.refresh_token)
-			.then(esiRes => {
-				if(esiRes.status === 200 && esiRes.statusText === "OK") {
-					req.session.esi = esiRes.data;
-					req.session.esi.expiryTime = addSeconds(new Date(), esiRes.data.expires_in);
-
-					next();
-
-				} else {
-					throw new Error("Error requesting access token.");
-				}
-			})
-			.catch(err => {
-				console.error(err);
-				res.status(500).send(err.message);
-			});
-
+			throw new Error("Error requesting access token.");
 		}
+	})
+	.catch(err => {
+		console.error(err);
+		res.status(500).send(err.message);
+	});
+};
+
+const isAuthenticated = (req, res, next) => {
+	if(req.session.esi) {
+		next();
+	} else {
+		res.sendStatus(403);
 	}
 };
 
-router.use(accessTokenIsValid);
+const isAuthExpired = (req, res, next) => {
+	const msRemaining = calcRemainingTime(new Date(req.session.esi.expiryTime));
+
+	if(msRemaining > 60000) {
+		next();
+	} else {
+		refreshAuth(req, res, next);
+	}
+};
+
+router.use(isAuthenticated);
+router.use(isAuthExpired);
 
 router.use("/assets", esiAssets);
 router.use("/wallet", esiWallet);
