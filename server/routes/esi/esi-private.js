7e74fefd4eb239f5f5afd84301b4b5b20e7ab78d
jlyonas <jlyon@acumensolutions.com>
2018-01-26 09:01:44 -0500

    check on req.session.esi log

--- server/routes/esi/esi-private.js
+++ server/routes/esi/esi-private.js
@@ -4,35 +4,38 @@ const esiAssets = require("./esi-assets");
 const esiWallet = require("./esi-wallet");
 
 const { addSeconds, calcRemainingTime } = require("../../plugins/date-helper");
-const esiAuthApi = require("../../esi/esi-auth");
+const esiApi = require("../../esi/esi-auth");
 
 const accessTokenIsValid = (req, res, next) => {
 	console.log("*** confirming token validity ***");
 	if(req.session.esi) {
+		console.log("*** has session.esi ***");
 
 		const msRemaining = calcRemainingTime(new Date(req.session.esi.expiryTime));
 
 		if(msRemaining <= 60000) {
 
-			esiAuthApi.refreshAccess(req.session.esi.refresh_token)
-				.then(esiRes => {
-					if(esiRes.status === 200 && esiRes.statusText === "OK") {
-						req.session.esi = esiRes.data;
-						req.session.esi.expiryTime = addSeconds(new Date(), esiRes.data.expires_in);
+			esiApi.refreshAccess(req.session.esi.refresh_token)
+			.then(esiRes => {
+				if(esiRes.status === 200 && esiRes.statusText === "OK") {
+					req.session.esi = esiRes.data;
+					req.session.esi.expiryTime = addSeconds(new Date(), esiRes.data.expires_in);
 
-						next();
+					next();
 
-					} else {
-						throw new Error("Error requesting access token.");
-					}
-				})
-				.catch(err => {
-					console.error(err);
-					next(err);
-				});
+				} else {
+					throw new Error("Error requesting access token.");
+				}
+			})
+			.catch(err => {
+				console.error(err);
+				next(err);
+			});
+
 		} else {
 			next();
 		}
+
 	} else {
 		next();
 	}
